f8c2885c4ba945fef0eae96484060fbb118e0cf6
lykycy123 <1336857701@qq.com>
2024-10-28 16:25:13 +0800

    callbcak_1

--- src/arm_control/src/follow_joint_trajectory.cpp
+++ src/arm_control/src/follow_joint_trajectory.cpp
@@ -13,7 +13,7 @@ typedef actionlib::SimpleActionServer<control_msgs::FollowJointTrajectoryAction>
 // 用于存储 moveit 发送出来的轨迹数据
 moveit_msgs::RobotTrajectory moveit_tra;
 
-void execute_callback(const control_msgs::FollowJointTrajectoryGoalConstPtr& goalPtr, Server* moveit_server)
+void execute_callback(const control_msgs::FollowJointTrajectoryGoalConstPtr& goalPtr, Server* moveit_server, ros::Publisher &pub)
 {
     // 1、解析提交的目标值
     int n_joints = goalPtr->trajectory.joint_names.size();
@@ -47,6 +47,22 @@ void execute_callback(const control_msgs::FollowJointTrajectoryGoalConstPtr& goa
     ROS_INFO("Receive trajectory successfully");
     moveit_server->setSucceeded();
 
+    arm_control::JointControl msg;
+    for(int i = 0; i < moveit_tra.joint_trajectory.points.size(); i++)
+    {
+        for(int j = 0; j < 6; j++)
+        {
+            msg.joint_pos[j] = moveit_tra.joint_trajectory.points[i].positions[j];
+            msg.joint_vel[j] = 0.0;
+            msg.joint_cur[j] = 0.0;
+
+            printf("msg.joint_pos: %f \n", msg.joint_pos[j]);
+        }
+        sleep(1);
+        printf("msg\n");
+        pub.publish(msg);
+    }
+
 
 }
 
@@ -59,35 +75,13 @@ int main(int argc, char *argv[])
     ros::Publisher pub = nh.advertise<arm_control::JointControl>("JointControl", 10);
 
     // 创建 action 对象(NodeHandle，话题名称，回调函数解析传入的目标值，服务器是否自启动)
-    Server moveit_server(nh,"arx5_arm_controller/follow_joint_trajectory", boost::bind(&execute_callback, _1, &moveit_server), false);
+    Server moveit_server(nh,"arx5_arm_controller/follow_joint_trajectory", boost::bind(&execute_callback, _1, &moveit_server, pub), false);
     // 手动启动服务器
     moveit_server.start();
         
-    ros::Rate loop_rate(1);
 
-    while (ros::ok())
-    {
+    ros::spin();
 
-        arm_control::JointControl msg;
-        for(int i = 0; i < moveit_tra.joint_trajectory.points.size(); i++)
-        {
-            for(int j = 0; j < 6; j++)
-            {
-                msg.joint_pos[j] = moveit_tra.joint_trajectory.points[i].positions[j];
-                msg.joint_vel[j] = 0.1;
-                msg.joint_cur[j] = 0.1;
-
-                printf("msg.joint_pos: %f \n", msg.joint_pos[j]);
-            }
-
-            printf("msg\n");
-            pub.publish(msg);
-        }
-
-        ros::spin();
-
-        loop_rate.sleep();
-    }
     return 0;
 
 }
